32ea68efe1d84cc590b4067565a28115f3278dd5
hokmiran <hokmiransoltanli1996@gmail.com>
2023-09-30 12:50:36 +0400

    structure

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import "./assets/css/main.css";
+import "./assets/css/style.css";
+import "./assets/css/feather.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,9 +1,55 @@
+import './Login.scss';
 
-
-const Login = () => {
+function Login() {
   return (
-    <div>Login</div>
-  )
+    // <div className="columns ">
+    //   <div className="login-box">
+    //     <form className="form">
+    //       <div className="signin">
+    //         <h1 className="topline">Log in to your account</h1>
+    //         <br />
+    //         <div className="input-field">
+    //           <input
+    //             id="email"
+    //             type="text"
+    //             name="email"
+    //             placeholder="* Email"
+    //           />
+    //         </div>
+
+    //         <div className="input-field">
+    //           <input id="password" name="password" placeholder="* Password" />
+    //           <div className="password-toggle-button">
+    //             {/* <i
+    //                 className={showPassword ? "fas fa-eye-slash" : "fas fa-eye"}
+    //               ></i> */}
+    //           </div>
+    //         </div>
+    //         <div className="forgot-password-link">
+    //           <a href="/forgot-password">Forgot Password?</a>
+    //         </div>
+    //         <div className="login-box-button">
+    //           <button type="submit" style={{ cursor: "pointer" }}>
+    //             <i className="fas fa-sign-in-alt"></i>
+    //             <span style={{ marginLeft: "0.5rem", fontWeight: "normal" }}>
+    //               Log In
+    //             </span>
+    //           </button>
+    //         </div>
+    //       </div>
+    //     </form>
+    //   </div>
+    // </div>
+
+    <div className="loginBox">
+      <div>
+
+      </div>
+      <div>
+
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
